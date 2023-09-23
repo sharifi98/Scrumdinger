829f8c158e4d4e61b3998e8c39ec983958fe439c
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-09-23 14:52:49 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Hossein Sharifi on 23/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
