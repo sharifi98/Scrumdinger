5ce5c8d7f16798eb7533f2bf958c3d1649a17ced
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-10-09 12:03:22 +0200

    started the project SCRUMDINGER wich is a CRUD App

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,49 @@
+//
+//  MeetingView.swift
+//  Scrumdinger
+//
+//  Created by Hossein Sharifi on 09/10/2023.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 10, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("Seconds remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
