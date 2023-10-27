29dc3efab67f5a067bc81f3bcd93668522fba498
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-10-27 10:11:44 +0200

    started on the passing data with bindings

---
+++ Scrumdinger/ThemeView.swift
@@ -0,0 +1,25 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Hossein Sharifi on 27/10/2023.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundStyle(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+#Preview {
+    ThemeView(theme: .buttercup)
+}
